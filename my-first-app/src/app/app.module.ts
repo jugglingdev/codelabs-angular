7658ca37d96ac0272ae6b788f5dd3702b4ba7a3c
jugglingdev <kaylamarie1785@gmail.com>
2023-09-30 12:01:07 -0500

    Completed 'Splitting Apps into Components'

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,22 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert-component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    DataBindingComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
@@ -25,5 +20,4 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
