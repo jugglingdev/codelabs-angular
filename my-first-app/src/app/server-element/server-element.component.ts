14a9b0b55ab9d315c1fa6c899da4f42299385231
jugglingdev <kaylamarie1785@gmail.com>
2023-09-30 17:26:16 -0500

    Completed 'Getting Access to ng-content with @ContentChild' and 'Wrap Up'

--- my-first-app/src/app/server-element/server-element.component.ts
+++ my-first-app/src/app/server-element/server-element.component.ts
@@ -12,7 +12,8 @@ import {
   AfterViewChecked,
   OnDestroy,
   ViewChild,
-  ElementRef
+  ElementRef,
+  ContentChild
 } from '@angular/core';
 
 @Component({
@@ -34,6 +35,7 @@ export class ServerElementComponent implements
   @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name: string;
   @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
   constructor() {
     console.log('constructor called!');
@@ -47,6 +49,8 @@ export class ServerElementComponent implements
   ngOnInit() {
     console.log('ngOnInit() called!');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+
   }
 
   ngDoCheck() {
@@ -55,6 +59,7 @@ export class ServerElementComponent implements
 
   ngAfterContentInit() {
     console.log('ngAfterContentInit() called!');
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked() {
