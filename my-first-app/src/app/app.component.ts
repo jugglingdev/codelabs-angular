46bc4117192519a4cece0c4003b34cddc36cf052
jugglingdev <kaylamarie1785@gmail.com>
2023-10-21 22:29:12 -0500

    Completed 'Introduction to the Reactive Approach' and 'Reactive: Setup'

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +7,6 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  subscriptions = ['Basic', 'Advanced', 'Pro'];
-  selectedSubscription = 'Advanced';
-  @ViewChild('signupForm') sgnForm: NgForm;
-
-  onSubmit() {
-    console.log(this.sgnForm.value);
-  }
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
 }
