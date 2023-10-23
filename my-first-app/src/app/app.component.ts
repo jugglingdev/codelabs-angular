1ce09f53501b6def154095a9ee5efd72b16255ca
jugglingdev <kaylamarie1785@gmail.com>
2023-10-22 20:50:41 -0500

    Completed 'Reactive: Setting and Patching Values'

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -27,10 +27,26 @@ export class AppComponent implements OnInit {
       this.signupForm.statusChanges.subscribe(
         (status) => console.log(status)
       );
+
+      this.signupForm.setValue({
+        'userData': {
+          'username': 'Max',
+          'email': 'max@test.com'
+        },
+        'gender': 'male',
+        'hobbies': []
+      });
+      this.signupForm.patchValue({
+        'userData': {
+          'username': 'Anna',
+          'email': 'max@test.com'
+        }
+      });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
