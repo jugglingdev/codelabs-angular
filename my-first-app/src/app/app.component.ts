ca497e330cd2e85fda43cd3cdcb7480008a10094
jugglingdev <kaylamarie1785@gmail.com>
2023-10-22 20:32:04 -0500

    Completed 'Reactive: Using Error Code'

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
       this.signupForm = new FormGroup({
         'userData': new FormGroup({
-          'username': new FormControl(null, Validators.required, this.forbiddenNames.bind(this)),
+          'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
           'email': new FormControl(null, [Validators.required, Validators.email])
         }),
         'gender': new FormControl('male'),
@@ -38,7 +38,7 @@ export class AppComponent implements OnInit {
 
   forbiddenNames(control: FormControl): {[s: string]: boolean} {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
-      return {'namesIsForbidden': true};
+      return {'nameIsForbidden': true};
     }
     return null;
   }
