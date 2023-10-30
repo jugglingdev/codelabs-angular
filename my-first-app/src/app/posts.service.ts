9d4b2fb190b91956efad1da9e5aedc0f48bf5172
jugglingdev <kaylamarie1785@gmail.com>
2023-10-30 16:56:25 -0500

    Completed 'Sending a DELETE Request'

--- my-first-app/src/app/posts.service.ts
+++ my-first-app/src/app/posts.service.ts
@@ -35,4 +35,8 @@ export class PostsService {
         })
       );
   }
+
+  deletePosts() {
+    return this.http.delete('https://ng-complete-guide-719cf-default-rtdb.firebaseio.com/posts.json')
+  }
 }
