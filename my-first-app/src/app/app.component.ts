b201fd3609e8886cb71069b5daa05031ee3e601a
jugglingdev <kaylamarie1785@gmail.com>
2023-10-29 22:50:01 -0500

    Completed 'The Anatomy of a Http Request', 'Backend (Firebase) Setup', and 'Sending a POST Request'

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -15,7 +15,13 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http
+      .post(
+        'https://ng-complete-guide-719cf-default-rtdb.firebaseio.com/posts.json',
+        postData
+      ).subscribe(responseData => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
